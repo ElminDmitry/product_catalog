11008db34d078135e3fd0238dcf747ad02362632
ElminDmitry <elmin.mailbox@gmail.com>
2018-02-05 15:44:39 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-  Product.create(
-    name: 'Dirt',
-    sold_out: true,
-    category: 'History',
-    under_sale: true,
-    price: rand(2000),
-    sale_price: rand(1000...2000)
-  )
+5.times do
+  Product.create(name: Faker::Movie.quote,
+                 sold_out: true,
+                 category: 'Movies',
+                 under_sale: true,
+                 price: rand(2000),
+                 sale_price: rand(1000...2000))
+end
+
+5.times do
+  Product.create(name: Faker::Commerce.material,
+                 sold_out: true,
+                 category: 'Materials',
+                 under_sale: true,
+                 price: rand(2000),
+                 sale_price: rand(1000...2000))
 end
